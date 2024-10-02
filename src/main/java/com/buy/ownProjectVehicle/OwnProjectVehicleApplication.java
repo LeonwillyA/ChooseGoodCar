a6dfa9459526348c9ba90076b49577826c98f101
LeonwillyA <leonalvaradowilly@gmail.com>
2024-10-02 10:20:30 -0500

    Primer commit, para colgar mi avance a la pagina gratuita de GITHUB

---
+++ src/main/java/com/buy/ownProjectVehicle/OwnProjectVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.buy.ownProjectVehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnProjectVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnProjectVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buy/ownProjectVehicle/OwnProjectVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buy.ownProjectVehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnProjectVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
